4261a2609280c825d85feb16c7d44e4706265825
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-02-17 18:21:02 +0100

    feat(api): create column definitions and insert new contacts

--- api/app/Controllers/Http/ContactsController.ts
+++ api/app/Controllers/Http/ContactsController.ts
@@ -1,3 +1,101 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Contact from 'App/Models/Contact'
 
-export default class ContactsController {}
+export default class ContactsController {
+    public async index({}: HttpContextContract) {}
+
+    /**
+     * method for POST request
+     */
+    public async store({request, response}: HttpContextContract) {
+        const payload = request.body();
+
+        /**
+         * It is important to destructure and obtain the
+         * individual properties which we want to insert
+         * into our the table.
+         * 
+         * You do not want malicious actors to try to
+         * overwrite the `id` or `created_at` or
+         * `updated_at` by including those in the payload
+         *
+         * So, the statement below is discouraged as it is
+         * dangerous
+         * 
+         * `await Contact.create(payload)`
+         *
+         * Again, destructure the `payload` as done
+         * below, no matter how many properties are there
+         * in the payload
+         */
+        const {
+            firstName,
+            surname,
+            company,
+            jobTitle,
+            email1,
+            email2,
+            phoneNumber1,
+            phoneNumber2,
+            country,
+            streetAddressLine1,
+            streetAddressLine2,
+            city,
+            postCode,
+            state,
+            birthday,
+            website,
+            notes,
+        } = payload
+
+        /**
+         * create a new contact
+         */
+        const contact = await Contact.create({
+            firstName,
+            surname,
+            company,
+            jobTitle,
+            email1,
+            email2,
+            phoneNumber1,
+            phoneNumber2,
+            country,
+            streetAddressLine1,
+            streetAddressLine2,
+            city,
+            postCode,
+            state,
+            birthday,
+            website,
+            notes,
+        })
+
+        /**
+         * Refreshing the `contact moedl before returning
+         * it as the payload of the response is important
+         * 
+         * If not refreshed, only the actual/few properties
+         * which were inserted will be returned
+         */
+
+        await contact.refresh()
+
+        return response.created(contact)    
+    }
+
+    /**
+     * ###
+     */
+    public async show({}: HttpContextContract) {}
+
+    /**
+     * ###
+     */
+    public async update({}: HttpContextContract) {}
+
+    /**
+     * ###
+     */
+    public async destroy({}: HttpContextContract) {}
+}
--- api/app/Models/Contact.ts
+++ api/app/Models/Contact.ts
@@ -1,13 +1,82 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import {cuid} from '@ioc:Adonis/Core/Helpers'
 
 export default class Contact extends BaseModel {
+  public static selfAssignPrimaryKey = true
+
   @column({ isPrimary: true })
-  public id: number
+  public id: string
+
+  @column()
+  public firstName: string
+
+  @column()
+  public surname: string
+
+  @column()
+  public company?: string | null | undefined
+
+  @column()
+  public jobTitle?: string | null | undefined
+
+  @column()
+  public email1: string
+
+  @column()
+  public email2?: string | null | undefined
+
+  @column()
+  public phoneNumber1: string
+
+  @column()
+  phoneNumber2?: string | null | undefined
+
+  @column()
+  public country?: string | null | undefined
+
+  @column()
+  public streetAddressLine1?: string | null | undefined
+  
+  @column()
+  public streetAddressLine2?: string | null | undefined
+
+  @column()
+  public city?: string | null | undefined
+
+  @column()
+  public postCode?: string | null | undefined
+
+  @column()
+  public state?: string | null | undefined
+
+  @column()
+  public birthday?: string | null | undefined
+  
+  @column()
+  public website?: string | null | undefined
+
+  @column()
+  public notes?: string | null | undefined
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  /**
+   * Runs before creating a new record
+   */
+  @beforeCreate()
+  public static generateUUID(contact: Contact): void {
+
+    /**
+     * This is a static method to generate a random ID
+     * for the `id` column
+     * 
+     * `cuid()` returns a collision-resistant unique ID
+     */
+    contact.id = cuid()
+  }
 }
